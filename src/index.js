72623c3b566cca20e0909d56e3aefcd7ed7adbed
Michael Esch <eschmichael@yandex.com>
2018-07-14 21:53:35 -0400

    Font

--- src/index.js
+++ src/index.js
@@ -3,16 +3,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { injectGlobal } from 'styled-components';
+import { injectGlobal, css } from 'styled-components';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+const cssImports = css`
+    @import url('https://fonts.googleapis.com/css?family=IBM+Plex+Sans+Condensed|IBM+Plex+Sans:400i');
+`;
+
 // Global CSS
 injectGlobal`
+    ${ () => cssImports}
     body {
         margin: 0;
         padding: 0;
-        font-family: sans-serif;
+        font-family: 'IBM Plex Sans Condensed', sans-serif;
     }
 `;
 
