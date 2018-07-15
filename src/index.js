5108665466eeb37d395c0f9ed14f2eeff3458891
Michael Esch <eschmichael@yandex.com>
2018-07-14 21:35:01 -0400

    Use styled-components injectGlobal method

--- src/index.js
+++ src/index.js
@@ -3,8 +3,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { injectGlobal } from 'styled-components';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+// Global CSS
+injectGlobal`
+    body {
+        margin: 0;
+        padding: 0;
+        font-family: sans-serif;
+    }
+`;
 
 ReactDOM.render(
     <BrowserRouter>
