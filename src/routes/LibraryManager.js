707d441094a43e85a6ca172d8c80eaa2e1b8b109
Michael Esch <eschmichael@yandex.com>
2018-07-14 22:41:13 -0400

    Setup filesync and lowdb

--- src/routes/LibraryManager.js
+++ src/routes/LibraryManager.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import LinkButton from '../components/LinkButton';
 // import { Link } from 'react-router-dom';
+import lowdb from 'lowdb';
+import FileSync from 'lowdb/adapters/FileSync';
+
+const adapter = new FileSync('db.json');
+const db = lowdb(adapter);
+
+// TODO: add code for adding symbols (svg's) to db
+// TODO: add code for removing / managing symbols
 
 const LibraryManager = () => (
     <div className="container"><h1>LibraryManager</h1>
